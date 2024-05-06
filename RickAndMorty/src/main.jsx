197ef881bc90d3408d5fa705cd2b0f7cc845bea1
LuLuMS0206 <luciamacho00@gmail.com>
2024-05-06 22:48:54 +0200

    add pages routes

--- RickAndMorty/src/main.jsx
+++ RickAndMorty/src/main.jsx
@@ -1,10 +1,17 @@
+/* eslint-disable react/jsx-no-undef */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
