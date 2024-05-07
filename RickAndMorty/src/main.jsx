6e0721e38ab21399bc8cc234168970e31ab61397
LuLuMS0206 <luciamacho00@gmail.com>
2024-05-07 11:41:28 +0200

    add page listpersonpage and route

--- RickAndMorty/src/main.jsx
+++ RickAndMorty/src/main.jsx
@@ -1,14 +1,16 @@
-/* eslint-disable react/jsx-no-undef */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {ListPersonPage} from './pages/listPersonPage'
+import {BrowserRouter, Routes,Route } from 'react-router-dom'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route />
+        <Route />
         <Route/>
         <Route path='personList' element= {<ListPersonPage/>} />
         <Route />
--- RickAndMorty/src/pages/listPersonPage.jsx
+++ RickAndMorty/src/pages/listPersonPage.jsx
@@ -1,9 +1,36 @@
-/* eslint-disable react/jsx-no-undef */
 
+import { useState, useEffect} from 'react'
+import { CardsComponent } from "../components/cardsComponent/cardsComponent"
 
 export const ListPersonPage = () => {
 
+
+    const [characters, setCharacters] = useState([]);
+
+    // con useEffect hago la llamada a la API y actualizo el estado characters con los resultados
+
+    useEffect(() => {
+        fetch('https://rickandmortyapi.com/api/character')
+            .then(response => response.json())
+            .then(data => setCharacters(data.results))
+    }, []);
+
+
+    // con el .map itero sobre el array
     return (
-        <CardsComponent/>
-    )
+        <div>
+            <h1>Listado de Personajes de Rick and Morty</h1>
+            <div className="cards-container">
+                {characters.map(character => (
+                    <CardsComponent
+                        key={character.id}
+                        name={character.name}
+                        image={character.image}
+                        species={character.species}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+
 }
