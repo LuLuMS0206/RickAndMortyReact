68eb0ab77d20f9c570711f413b01c8959516292c
LuLuMS0206 <luciamacho00@gmail.com>
2024-05-15 09:43:42 +0200

    add thunk

--- RickAndMorty/src/features/rickyAndMortyThunk.js
+++ RickAndMorty/src/features/rickyAndMortyThunk.js
@@ -2,5 +2,9 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 
 export const GetRickyAndMortyThunk = createAsyncThunk ('RickyAndMorty/getRickyAndMorty', async() =>{
-    const request = await fetch()
+    const request = await fetch('https://rickandmortyapi.com/api/character')
+    if (request.ok){
+        const data = await request.json
+        return data.data
+    }
 })
